8f6dfe4820b6fedfc0cb4e31740f169848f1e97e
Bcuzeau <73339099+Bcuzeau@users.noreply.github.com>
2022-08-20 00:18:31 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,6 @@ let strip = neopixel.create(DigitalPin.P2, 3, NeoPixelMode.RGB)
 let color = 0
 basic.forever(function () {
     color += 1
-    strip.showRainbow(color, 270)
+    strip.showRainbow(color, color + 270)
     basic.pause(20)
 })
